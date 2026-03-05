ec6816a67daf1cadcc8b9e6bb1f50ecca94a6f4c
Viraj-001 <viraj.kolhe.batch2025@sitnagpur.siu.edu.in>
2026-03-05 14:29:31 +0530

    Update Merge_Sort.c

--- Experiment3/Merge_Sort.c
+++ Experiment3/Merge_Sort.c
@@ -1,6 +1,39 @@
-
 #include <stdio.h>
 
+// Function to merge two subarrays
+void merge(int arr[], int left, int mid, int right) {
+int n1=mid-left+1;
+	int n2=right-mid;
+	int L[n1];
+	int R[n2];
+	for(int i=0; i<n1; i++){
+		L[i]=arr[left+i];
+	}
+	for(int j=0; j<n2; j++){
+		R[j]=arr[mid+1+j];
+	}
+	int i=0,j=0,k=left;
+	while(i<n1 && j<n2){
+		if(L[i]<=R[j]){
+			arr[k]=L[i];
+			i++;
+		}else{
+			arr[k]=R[j];
+			j++;
+		}
+		k++;
+	}
+	while(i<n1){
+		arr[k]=L[i];
+		i++;
+		k++;
+	}
+	while(j<n2){
+		arr[k]=R[j];
+		j++;
+		k++;
+	}
+		// write your code here
 
 
 
@@ -8,17 +41,13 @@
 
 // Function to implement merge sort
 void mergeSort(int arr[], int left, int right) {
-
-  if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
-
-
+if(left<right){
+	int mid=(left+right)/2;
+	mergeSort(arr, left, mid);
+	mergeSort(arr, mid+1, right);
+	merge(arr, left, mid, right);
+}
+  // write your code here
   
   
   
@@ -27,14 +56,11 @@ void mergeSort(int arr[], int left, int right) {
 
 // Function to print an array
 void printArray(int arr[], int size) {
-
-for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-
-
+for(int i=0;i<size;i++){
+	printf("%d ", arr[i]);
+}
+	printf("\n");
+// write your code here
 
 
 }
